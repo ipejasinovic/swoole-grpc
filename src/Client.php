20ba925457b8deec0a6dbee2fb85404f07ad93b5
Igor Pejasinovic <igor.p@aleapartners.io>
2023-04-04 17:17:40 +0200

    Fixed connection pool and add auto reconnect option

--- src/Client.php
+++ src/Client.php
@@ -7,187 +7,186 @@ declare(strict_types=1);
  * @contact  hello@openswoole.com
  * @license  https://github.com/openswoole/grpc/blob/main/LICENSE
  */
+
 namespace OpenSwoole\GRPC;
 
 use OpenSwoole\GRPC\Exception\ClientException;
 
-class Client implements ClientInterface
-{
-    private $client;
+class Client implements ClientInterface {
 
-    private $streams;
+	private $client;
+	private $streams;
+	private $closed = false;
+	private $host;
+	private $port;
+	private $mode;
+	private $settings = [
+		'timeout' => 3,
+		'open_eof_check' => true,
+		'package_max_length' => 2 * 1024 * 1024,
+		'http2_max_concurrent_streams' => 1000,
+		'http2_max_frame_size' => 2 * 1024 * 1024,
+		'max_retries' => 10,
+		'force_reconnect' => false,
+		'receive_timeout' => -1
+	];
 
-    private $closed = false;
+	public function __construct($host, $port, $mode = Constant::GRPC_CALL, $settings = []) {
+		// TODO: clientInterceptors
+		$this->streams = [];
+		$this->mode = $mode;
+		$this->host = $host;
+		$this->port = $port;
+		$this->settings = array_merge($this->settings, $settings);
+		$this->client = new \Swoole\Coroutine\Http2\Client($this->host, $this->port);
+		return $this;
+	}
 
-    private $mode;
+	public function set(array $settings): self {
+		$this->settings = array_merge($this->settings, $settings ?? []);
+		return $this;
+	}
 
-    private $settings = [
-        'timeout'                      => 3,
-        'open_eof_check'               => true,
-        'package_max_length'           => 2 * 1024 * 1024,
-        'http2_max_concurrent_streams' => 1000,
-        'http2_max_frame_size'         => 2 * 1024 * 1024,
-        'max_retries'                  => 10,
-    ];
+	/**
+	 * Esbalish a connection to the remote endpoint
+	 */
+	public function connect(): self {
+		$this->client->set($this->settings);
+		if (!$this->client->connect()) {
+			throw new ClientException(swoole_strerror($this->client->errCode, 9) . " {$this->client->host}:{$this->client->port}", $this->client->errCode);
+		}
+		\Swoole\Coroutine::create(function () {
+			while (!$this->closed && [$streamId, $data, $pipeline, $trailers] = $this->recvData()) {
+				if ($streamId > 0 && !$pipeline) {
+					$this->streams[$streamId][0]->push([$data, $trailers]);
+					$this->streams[$streamId][0]->close();
+					unset($this->streams[$streamId]);
+				} elseif ($streamId > 0) {
+					$this->streams[$streamId][0]->push([$data, $trailers]);
+				}
+			}
+		});
+		return $this;
+	}
 
-    public function __construct($host, $port, $mode = Constant::GRPC_CALL, $settings = [])
-    {
-        $client = new \Swoole\Coroutine\Http2\Client($host, $port);
-        // TODO: clientInterceptors
-        $this->client  = $client;
-        $this->streams = [];
-        $this->mode    = $mode;
-        $this->settings = array_merge($this->settings, $settings);
-        return $this;
-    }
+	/**
+	 * Get the stats of the client
+	 */
+	public function stats(): array {
+		return $this->client->stats();
+	}
 
-    public function set(array $settings): self
-    {
-        $this->settings = array_merge($this->settings, $settings ?? []);
-        return $this;
-    }
+	/**
+	 * Close the connection to the remote endpoint
+	 */
+	public function close() {
+		$this->closed = true;
+		$this->client->close();
+	}
 
-    /**
-     * Esbalish a connection to the remote endpoint
-     */
-    public function connect(): self
-    {
-        $this->client->set($this->settings);
-        if (!$this->client->connect()) {
-            throw new ClientException(swoole_strerror($this->client->errCode, 9) . " {$this->client->host}:{$this->client->port}", $this->client->errCode);
-        }
+	/**
+	 * Send message to remote endpoint, either end the stream or not depending on $mode of the client
+	 * @param mixed $method
+	 * @param mixed $message
+	 * @param mixed $type
+	 */
+	public function send($method, $message, $type = 'proto') {
+		$isEndStream = $this->mode === Constant::GRPC_CALL;
+		$retry = 0;
+		while ($retry++ < $this->settings['max_retries']) {
+			$streamId = $this->sendMessage($method, $message, $type);
+			if ($streamId && $streamId > 0) {
+				$this->streams[$streamId] = [new \Swoole\Coroutine\Channel(1), $isEndStream];
+				return $streamId;
+			}
+			if ($this->client->errCode > 0) {
+				if (in_array($this->client->errCode, [32, 111, 5001]) && $this->settings['force_reconnect']) {
+					$this->client->close();
+					$this->client->connect();
+					continue;
+				}
+				throw new ClientException(swoole_strerror($this->client->errCode, 9) . " {$this->client->host}:{$this->client->port}", $this->client->errCode);
+			}
+			\Swoole\Coroutine::usleep(10000);
+		}
+		return false;
+	}
 
-        \Swoole\Coroutine::create(function () {
-            while (!$this->closed && [$streamId, $data, $pipeline, $trailers] = $this->recvData()) {
-                if ($streamId > 0 && !$pipeline) {
-                    $this->streams[$streamId][0]->push([$data, $trailers]);
-                    $this->streams[$streamId][0]->close();
-                    unset($this->streams[$streamId]);
-                } elseif ($streamId > 0) {
-                    $this->streams[$streamId][0]->push([$data, $trailers]);
-                }
-            }
-        });
-        return $this;
-    }
+	/**
+	 * Receive the data from a stream in the established connection based on streamId.
+	 * @param mixed $streamId
+	 * @param mixed $timeout
+	 */
+	public function recv($streamId, $timeout = -1) {
+		if ($this->streams[$streamId] ?? false) {
+			$data = $this->streams[$streamId][0]->pop($this->settings['receive_timeout'] ?? $timeout);
+			if (!$data) {
+				return [null, ['grpc-status' => '4', 'grpc-message' => 'DEADLINE_EXCEEDED']];
+			}
+			return $data;
+		}
+		throw new ClientException(swoole_strerror(86, 9) . " {$this->client->host}:{$this->client->port}", 86);
+	}
 
-    /**
-     * Get the stats of the client
-     */
-    public function stats(): array
-    {
-        return $this->client->stats();
-    }
+	/**
+	 * Push message to the remote endpoint, used in client side streaming mode.
+	 * @param mixed $streamId
+	 * @param mixed $message
+	 * @param mixed $type
+	 * @param bool $end
+	 */
+	public function push($streamId, $message, $type = 'proto', $end = false) {
+		if ($type === 'proto') {
+			$payload = $message->serializeToString();
+		} elseif ($type === 'json') {
+			$payload = $message;
+		}
+		$payload = pack('CN', 0, strlen($payload)) . $payload;
+		return $this->client->write($streamId, $payload, $end);
+	}
 
-    /**
-     * Close the connection to the remote endpoint
-     */
-    public function close()
-    {
-        $this->closed = true;
-        $this->client->close();
-    }
+	private function sendMessage($method, $message, $type) {
+		$request = new \Swoole\Http2\Request();
+		$request->pipeline = false;
+		$request->method = 'POST';
+		$request->path = $method;
+		$request->headers = [
+			'user-agent' => 'grpc-openswoole/' . \SWOOLE_VERSION,
+			'content-type' => 'application/grpc+' . $type,
+			'te' => 'trailers',
+		];
+		if ($type === 'proto') {
+			$payload = $message->serializeToString();
+		} elseif ($type === 'json') {
+			$payload = $message;
+		}
+		$request->data = pack('CN', 0, strlen($payload)) . $payload;
+		return $this->client->send($request);
+	}
 
-    /**
-     * Send message to remote endpoint, either end the stream or not depending on $mode of the client
-     * @param mixed $method
-     * @param mixed $message
-     * @param mixed $type
-     */
-    public function send($method, $message, $type = 'proto')
-    {
-        $isEndStream = $this->mode === Constant::GRPC_CALL;
-        $retry       = 0;
-        while ($retry++ < $this->settings['max_retries']) {
-            $streamId = $this->sendMessage($method, $message, $type);
-            if ($streamId && $streamId > 0) {
-                $this->streams[$streamId] = [new \Swoole\Coroutine\Channel(1), $isEndStream];
-                return $streamId;
-            }
-            if ($this->client->errCode > 0) {
-                throw new ClientException(swoole_strerror($this->client->errCode, 9) . " {$this->client->host}:{$this->client->port}", $this->client->errCode);
-            }
-            \Swoole\Coroutine::usleep(10000);
-        }
-        return false;
-    }
+	private function recvData() {
+		if ($this->mode === Constant::GRPC_CALL) {
+			$response = $this->client->recv($this->settings['timeout'] * $this->settings['max_retries']);
+		} else {
+			$response = $this->client->read($this->settings['timeout'] * $this->settings['max_retries']);
+		}
+		if (!$response) {
+			if ($this->client->errCode > 0) {
+				//throw new ClientException(swoole_strerror($this->client->errCode, 9) . " {$this->client->host}:{$this->client->port}", $this->client->errCode);
+			}
+			\Swoole\Coroutine::sleep(1);
+			return [0, null, false, null];
+		}
+		if ($response) {
+			if ($response->data) {
+				$data = substr($response->data, 5);
+			} else {
+				$data = '';
+			}
+			$trailers = ['grpc-status' => $response->headers['grpc-status'] ?? '0', 'grpc-message' => $response->headers['grpc-message'] ?? ''];
+			return [$response->streamId, $data, $response->pipeline, $trailers];
+		}
+		return [0, null, false, null];
+	}
 
-    /**
-     * Receive the data from a stream in the established connection based on streamId.
-     * @param mixed $streamId
-     * @param mixed $timeout
-     */
-    public function recv($streamId, $timeout = -1)
-    {
-        return $this->streams[$streamId][0]->pop($timeout);
-    }
-
-    /**
-     * Push message to the remote endpoint, used in client side streaming mode.
-     * @param mixed $streamId
-     * @param mixed $message
-     * @param mixed $type
-     * @param bool $end
-     */
-    public function push($streamId, $message, $type = 'proto', $end = false)
-    {
-        if ($type === 'proto') {
-            $payload = $message->serializeToString();
-        } elseif ($type === 'json') {
-            $payload = $message;
-        }
-        $payload = pack('CN', 0, strlen($payload)) . $payload;
-        return $this->client->write($streamId, $payload, $end);
-    }
-
-    private function sendMessage($method, $message, $type)
-    {
-        $request           = new \Swoole\Http2\Request();
-        $request->pipeline = false;
-        $request->method   = 'POST';
-        $request->path     = $method;
-        $request->headers  = [
-            'user-agent'     => 'grpc-openswoole/' . \SWOOLE_VERSION,
-            'content-type'   => 'application/grpc+' . $type,
-            'te'             => 'trailers',
-        ];
-        if ($type === 'proto') {
-            $payload = $message->serializeToString();
-        } elseif ($type === 'json') {
-            $payload = $message;
-        }
-        $request->data = pack('CN', 0, strlen($payload)) . $payload;
-
-        return $this->client->send($request);
-    }
-
-    private function recvData()
-    {
-        if ($this->mode === Constant::GRPC_CALL) {
-            $response = $this->client->recv($this->settings['timeout'] * $this->settings['max_retries']);
-        } else {
-            $response = $this->client->read($this->settings['timeout'] * $this->settings['max_retries']);
-        }
-        
-        if (!$response) {
-            if ($this->client->errCode > 0) {
-                // throw new ClientException(swoole_strerror($this->client->errCode, 9) . " {$this->client->host}:{$this->client->port}", $this->client->errCode);
-            }
-            \Swoole\Coroutine::sleep(1);
-            return [0, null, false, null];
-        }
-
-        if ($response) {
-            if ($response->data) {
-                $data = substr($response->data, 5);
-            } else {
-                $data = '';
-            }
-            $trailers = ['grpc-status' => $response->headers['grpc-status'] ?? '0', 'grpc-message' => $response->headers['grpc-message'] ?? ''];
-
-            return [$response->streamId, $data, $response->pipeline, $trailers];
-        }
-
-        return [0, null, false, null];
-    }
 }
--- src/ClientPool.php
+++ src/ClientPool.php
@@ -7,97 +7,102 @@ declare(strict_types=1);
  * @contact  hello@openswoole.com
  * @license  https://github.com/openswoole/grpc/blob/main/LICENSE
  */
+
 namespace OpenSwoole\GRPC;
 
-class ClientPool
-{
-    public const DEFAULT_SIZE = 16;
+class ClientPool {
 
-    private $pool;
+	public const DEFAULT_SIZE = 16;
 
-    private $size;
+	private $pool;
+	private $size;
+	private $num;
+	private $used;
+	private $factory;
+	private $config;
+	private $settings = [
+		'force_reconnect' => false,
+		'receive_timeout' => -1
+	];
 
-    private $num;
+	public function __construct($factory, $config, int $size = self::DEFAULT_SIZE, $settings = []) {
+		$this->pool = new \Swoole\Coroutine\Channel($this->size = $size);
+		$this->num = 0;
+		$this->factory = $factory;
+		$this->config = $config;
+		$this->settings = $settings;
+	}
 
-    private $active;
+	public function fill(): void {
+		while ($this->size > $this->num) {
+			$this->make();
+		}
+	}
 
-    private $factory;
+	public function get(float $timeout = -1) {
+		if ($this->pool->isEmpty() && $this->num < $this->size) {
+			go(function () {
+				while (!$this->make()) {
+					if (!$this->settings['force_recreate']) {
+						break;
+					}
+					\co::sleep(0.5);
+				}
+			});
+		}
+		$conn = $this->pool->pop($this->settings['receive_timeout'] > -1 ? $this->settings['receive_timeout'] : $timeout);
+		if ($conn) {
+			$this->used++;
+		}
+		return $conn;
+	}
 
-    private $config;
-    
-    private $settings;
+	public function put($connection, $isNew = false): void {
+		if ($this->pool === null) {
+			return;
+		}
+		if ($connection !== null) {
+			$this->pool->push($connection);
+			if (!$isNew) {
+				$this->used--;
+			}
+		} else {
+			$this->num -= 1;
+			$this->make();
+		}
+	}
 
-    public function __construct($factory, $config, int $size = self::DEFAULT_SIZE, $settings = [])
-    {
-        $this->pool    = new \Swoole\Coroutine\Channel($this->size = $size);
-        $this->num     = 0;
-        $this->factory = $factory;
-        $this->config  = $config;
-        $this->settings = $settings;
-    }
+	public function close() {
+		if (!$this->pool) {
+			return false;
+		}
+		while (1) {
+			if ($this->used > 0) {
+				\co::sleep(0.5);
+				continue;
+			}
+			if (!$this->pool->isEmpty()) {
+				$client = $this->pool->pop();
+				$client->close();
+			} else {
+				break;
+			}
+		}
+		$this->pool->close();
+		$this->pool = null;
+		$this->num = 0;
+	}
 
-    public function fill(): void
-    {
-        while ($this->size > $this->num) {
-            $this->make();
-        }
-    }
+	protected function make() {
+		$this->num++;
+		try {
+			$client = $this->factory::make($this->config['host'], $this->config['port'], $this->settings)->connect();
+			$this->put($client, true);
+		} catch (\Exception $ex) {
+			$this->num--;
+			return false;
+		}
+		return true;
+	}
 
-    public function get(float $timeout = -1)
-    {
-        if ($this->pool->isEmpty() && $this->num < $this->size) {
-            $this->make();
-        }
-
-        $this->active++;
-
-        return $this->pool->pop($timeout);
-    }
-
-    public function put($connection, $isNew = false): void
-    {
-        if ($this->pool === null) {
-            return;
-        }
-        if ($connection !== null) {
-            $this->pool->push($connection);
-
-            if (!$isNew) {
-                $this->active--;
-            }
-        } else {
-            $this->num -= 1;
-            $this->make();
-        }
-    }
-
-    public function close()
-    {
-        if (!$this->pool) {
-            return false;
-        }
-        while (1) {
-            if ($this->active > 0) {
-                \co::sleep(1);
-                continue;
-            }
-            if (!$this->pool->isEmpty()) {
-                $client = $this->pool->pop();
-                $client->close();
-            } else {
-                break;
-            }
-        }
-
-        $this->pool->close();
-        $this->pool = null;
-        $this->num  = 0;
-    }
-
-    protected function make()
-    {
-        $this->num++;
-        $client = $this->factory::make($this->config['host'], $this->config['port'], $this->settings)->connect();
-        $this->put($client, true);
-    }
 }
